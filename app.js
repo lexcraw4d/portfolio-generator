606d81e8e0c05392a3a01359d94c5fa27c31a7ee
Lexie Crawford <lexcraw4d@gmail.com>
2021-05-15 19:04:23 -0400

    added prompt for name

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
-const fs = require('fs');
+// const fs = require('fs');
+const inquirer = require('inquirer');
 
-const profileDataArgs = process.argv.slice(2);
+// const generatePage = require('./src/page-template.js');
 
-const generatePage = require('./src/page-template.js');
+// fs.writeFile('./index.html', generatePage(name, github), (err) => {
+// 	if (err) throw new Error(err);
 
-const [name, github] = profileDataArgs;
-
-fs.writeFile('./index.html', generatePage(name, github), (err) => {
-	if (err) throw new Error(err);
-
-	console.log('Portfolio complete! Check out index.html to see the output!');
-});
+// 	console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+inquirer
+.prompt ([
+	{
+		type: 'input',
+		name: 'name',
+		message: 'What is your name?'
+	}
+]).then(
+	answers => console.log(answers)
+)
